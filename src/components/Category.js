c4c06c9df97e9f2885548788d7f697b3f11121ac
yourena <2373638339@qq.com>
2017-12-07 11:09:07 +0800

    添加首页分类

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,12 +4,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
-import item1 from './../image/1.jpg'
 import item2 from './../image/2.jpg'
 import item3 from './../image/3.jpg'
 import item4 from './../image/item4.jpg'
 import item5 from './../image/5.jpg'
-import mac from './../image/mac.png'
+import axios from 'axios'
+import configJson from 'configJson' ;
 import './category.less'
 export default class Category extends React.Component {
 
@@ -18,75 +18,64 @@ export default class Category extends React.Component {
         //构造函数用法
         //常用来绑定自定义函数，切记不要在这里或者组件的任何位置setState，state全部在reducer初始化，相信对开发的后期很有帮助
         this.state = {
-            data:[{index:0,image:item2,name:'EPB电子驻车制动系统',desc:'电动助力转向系统（Electric Power Steering），既节省能量，又保护了环境。'},
-            {index:1,image:item3,name:'EPB电子驻车制动系统',desc:'电动助力转向系统（Electric Power Steering），既节省能量，又保护了环境。'},
-            {index:2,image:item4,name:'EPB电子驻车制动系统',desc:'电动助力转向系统（Electric Power Steering），既节省能量，又保护了环境。'},
-            {index:6,image:item4,name:'EPB电子驻车制动系统',desc:'电动助力转向系统（Electric Power Steering），既节省能量，又保护了环境。'},
-            {index:7,image:item5,name:'EPB电子驻车制动系统',desc:'电动助力转向系统（Electric Power Steering），既节省能量，又保护了环境。'}],
-            tempData:[]
+            data: []
         }
     }
 
     componentDidMount() {
-        this.setState({
-            tempData:this.state.data
-        })
+        this.getInfo()
     }
-    clickCategory=()=>{
-        this.props.history.push('/')
-    }
-    showCategoryDetail=(index)=>{
-    }
-    render() {
-        const that=this;
-        const liMap=this.state.tempData.map(function (item,index) {
 
-            if(item.type==='detail'){
-                const detailliMap=item.products.map(function (item2,index2) {
-                    return(
-                        <li key={index2}>
-                            <h4>{item2.name}</h4>
-                            <div>{item2.desc}</div>
-                        </li>
-                    )
-                })
-                return(
-                    <div key={index} className="category-detail category-detail-show">
-                        <h3>{item.category}</h3>
-                        <ul>
-                            {detailliMap}
-                        </ul>
-                    </div>
-                )
-            }else{
+    getInfo = ()=> {
+        const that = this;
+        axios({
+            url: `${configJson.prefix}/classify`,
+            method: 'get',
+        })
+            .then(function (response) {
+                console.log(response);
+                if (response.data.status === 200) {
+                    that.setState({
+                        data: response.data.data.rows
+                    })
+                }
+            })
+            .catch(function (error) {
+                console.log(error)
+            });
+
+    }
+    clickCategory = (item)=> {
+        console.log('item',item)
+        this.props.history.push(`/products?q=${item.id}`)
+    }
+    showCategoryDetail = (index)=> {
+    }
+
+    render() {
+        const that = this;
+        const liMap = this.state.data.map(function (item, index) {
                 return (
-                    <li key={index}  className={item.active?"category-item active" :"category-item" } onClick={()=>that.showCategoryDetail(item.index)}>
+                    <li key={index} className={item.active ? "category-item active" : "category-item" }
+                        onClick={()=>that.showCategoryDetail(item.index)}>
                         <div className="category-img">
-                            <img src={item.image} alt=""/>
+                            <img src={`${configJson.prefix}${item.classifyUrl}`}  alt=""/>
                         </div>
                         <div className="category-name">
-                            <h3 onClick={()=>{that.props.history.push('/products')}} className="title" >{item.name}</h3>
-                            <p className="desc">{item.desc}</p>
-                            <Link to="/products">查看详情</Link>
+                            <h3 onClick={()=>{that.clickCategory(item)}} className="title">{item.name}</h3>
+                            <p className="desc">{item.description}</p>
+                            <Link to={`/products?q=${item.id}`}>查看详情</Link>
                         </div>
 
                     </li>
                 )
-            }
-
-
         })
         return (
             <div className="category-box ">
                 <div className="category">
-                    {this.props.showAll?
-                        <ul className="category-ul">
-                            {liMap}
-                        </ul>:
-                        <ul className="category-ul">
-                            {liMap}
-                        </ul>
-                    }
+                    <ul className="category-ul">
+                        {liMap}
+                    </ul>
 
                 </div>
             </div>
@@ -94,5 +83,4 @@ export default class Category extends React.Component {
         )
     }
 }
-Category.propTypes = {
-}
+Category.propTypes = {}
